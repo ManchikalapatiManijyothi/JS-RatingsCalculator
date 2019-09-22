cee7857c299d1febc7b4adc573b95cd5e78aff95
ManchikalapatiManijyothi <mani.manchikalapati@gmail.com>
2019-09-22 15:16:37 +0530

    Completed module

--- js/ratings.js
+++ js/ratings.js
@@ -1,5 +1,5 @@
 const collect_ratings=()=>{
-    const ratings={count=0,sum=0,average=0};
+    const ratings={'count':0,'sum':0,'average':0};
     let rating=0;
     const elements=document.querySelectorAll('.rating');
     elements.forEach(element=>{
@@ -7,10 +7,13 @@ const collect_ratings=()=>{
      ratings.count+=parseInt(element.value);
      ratings.sum+=parseInt(element.value)*rating;
      
-    })
+    });
     if(ratings.count!=0)
+    {
     ratings.average=ratings.sum/ratings.count;
+    }
     return ratings;
+
 }
 document.addEventListener('change',()=>{
     const ratings=collect_ratings();
